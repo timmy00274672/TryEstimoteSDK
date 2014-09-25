102c5528b96a7207e7bf09cbe31bf28e00d3ff9f
Timmy00274672 <Timmy00274672@gmail.com>
2014-09-25 18:12:38 +0800

    Add upload button and msg txt view without implementation

--- src/com/estimote/examples/demos/DipProjectActivity.java
+++ src/com/estimote/examples/demos/DipProjectActivity.java
@@ -1,5 +1,8 @@
 package com.estimote.examples.demos;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import android.app.Activity;
@@ -10,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.estimote.sdk.Beacon;
@@ -26,34 +30,70 @@ public class DipProjectActivity extends Activity {
 			.getSimpleName();
 	private BeaconManager beaconManager;
 	private boolean serviceRun = false;
-	private Button runButton;
+	private Button buttonRun;
+	private List<Beacon> beaconList = new ArrayList<Beacon>();
+	private TextView textViewNumber;
+	private Button buttonUpload;
+	private TextView textViewUploadMsg;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//set the view and actionbar and initialize the object in the view
+
+		// set the view and actionbar and initialize the object in the view
 		setContentView(R.layout.diplab_project);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		runButton = (Button)findViewById(R.id.runButton);
-		runButton.setOnClickListener(new OnClickListener() {
-			
+
+		buttonRun = (Button) findViewById(R.id.buttonRun);
+		buttonRun.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				toggleService();
 			}
 		});
-		
+
+		textViewNumber = (TextView) findViewById(R.id.textViewNumber);
+
 		beaconManager = new BeaconManager(this);
 		beaconManager.setRangingListener(new RangingListener() {
 
 			@Override
 			public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
 				Log.d(TAG, beacons.toString());
-
+				beaconList.addAll(beacons);
+				changeNumInTextViewNumber(beaconList.size());
 			}
 		});
+
+		buttonUpload = (Button) findViewById(R.id.buttonUpload);
+		buttonUpload.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				upload(beaconList);
+			}
+		});
+
+		textViewUploadMsg = (TextView) findViewById(R.id.textViewUploadMsg);
+
+	}
+
+	private void upload(List<Beacon> beaconList) {
+		// TODO use handler to do so
+		this.beaconList = new ArrayList<Beacon>();
+		changeNumInTextViewNumber(0);
+		changeNumInTextViewUploadMsg(beaconList.size());
+	}
+
+	private void changeNumInTextViewUploadMsg(int size) {
+		CharSequence msg = String.format("There are %d beacons upload", size);
+		textViewUploadMsg.setText(msg);
+	}
+
+	private void changeNumInTextViewNumber(int size) {
+		CharSequence msg = String.format("There are %d beacons", size);
+		textViewNumber.setText(msg);
 	}
 
 	@Override
@@ -63,9 +103,10 @@ public class DipProjectActivity extends Activity {
 
 			@Override
 			public void onServiceReady() {
-				Toast.makeText(DipProjectActivity.this, "Serivce is Ready", Toast.LENGTH_SHORT).show();
-				runButton.setText("START Service");
-				runButton.setEnabled(true);
+				Toast.makeText(DipProjectActivity.this, "Serivce is Ready",
+						Toast.LENGTH_SHORT).show();
+				buttonRun.setText("START Service");
+				buttonRun.setEnabled(true);
 			}
 		});
 	}
@@ -91,25 +132,28 @@ public class DipProjectActivity extends Activity {
 		super.onDestroy();
 	}
 
-	  @Override
-	  public boolean onOptionsItemSelected(MenuItem item) {
-	    if (item.getItemId() == android.R.id.home) {
-	      finish();
-	      
-	      return true;
-	    }
-	    return super.onOptionsItemSelected(item);
-	  }
-	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	private void toggleService() {
-		if(serviceRun){
+		if (serviceRun) {
 			stopRanging();
 			serviceRun = false;
-			runButton.setText("START Service");
-		}else{
+			// CharSequence msg = String.format("There are %d beacons",
+			// beaconList.size());
+			// Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+			buttonRun.setText("START Service");
+		} else {
 			startRanging();
 			serviceRun = true;
-			runButton.setText("STOP Service");
+			buttonRun.setText("STOP Service");
 		}
 	}
 
@@ -117,8 +161,7 @@ public class DipProjectActivity extends Activity {
 		try {
 			beaconManager.startRanging(ALL_ESTIMOTE_BEACONS_REGION);
 		} catch (RemoteException e) {
-			Toast.makeText(
-					DipProjectActivity.this,
+			Toast.makeText(DipProjectActivity.this,
 					"Cannot start ranging, something terrible happened",
 					Toast.LENGTH_LONG).show();
 			Log.e(TAG, "Cannot start ranging", e);
