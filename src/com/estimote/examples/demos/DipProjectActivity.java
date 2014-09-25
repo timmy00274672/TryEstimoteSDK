92fd196a76d520ced24ab5b75c07d9a6edbc3413
Timmy00274672 <Timmy00274672@gmail.com>
2014-09-25 20:07:57 +0800

    Finish the first version. Assume Scan_Id= 1

--- src/com/estimote/examples/demos/DipProjectActivity.java
+++ src/com/estimote/examples/demos/DipProjectActivity.java
@@ -1,6 +1,15 @@
 package com.estimote.examples.demos;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +17,9 @@ import org.xmlpull.v1.XmlSerializer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
 import android.os.RemoteException;
 import android.util.Log;
 import android.util.Xml;
@@ -90,62 +102,132 @@ public class DipProjectActivity extends Activity {
 	private void upload(List<Beacon> beaconList, int position) {
 		// TODO use handler to do so
 		{
-			XmlSerializer serializer = Xml.newSerializer();
-			StringWriter writer = new StringWriter();
-			try {
-				serializer.setOutput(writer);
-				serializer.startDocument("UTF-8", true);
-				serializer.startTag("", "data");
+			final String translateToXmlString = translateToXmlString(beaconList,
+					position);
+			Toast.makeText(this, translateToXmlString, Toast.LENGTH_LONG)
+					.show();
+			String response = "";
+			Log.d(TAG, translateToXmlString);
+			HandlerThread mHandlerThread = new HandlerThread("upload");
+			mHandlerThread.start();
+			Handler handler = new Handler(mHandlerThread.getLooper());
+			handler.post(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						HttpURLConnection httpUrlConnection = (HttpURLConnection) new URL(
+								"http://140.116.179.11/BT_project/BT_offline.php")
+								.openConnection();
+						httpUrlConnection.setConnectTimeout(20000);
+						httpUrlConnection.setDoOutput(true);
+						httpUrlConnection.setRequestMethod("POST");
+						httpUrlConnection
+								.setRequestProperty("Connection", "Keep-Alive");
+						httpUrlConnection.connect();
 
-				{
-					// <RP_Id>1</RP_Id>
-					serializer.startTag("", "RP_Id");
-					serializer.text(position + "");
-					serializer.endTag("", "RP_Id");
+						OutputStream os = httpUrlConnection.getOutputStream();
 
-					/*
-					 * <Bluetooth> <BeaconRecord>
-					 * <Mac_Address>DF:81:E2:C5:A2:BA</Mac_Address>
-					 * <RSS>-79</RSS> <Scan_Id>1</Scan_Id> </BeaconRecord>
-					 * <BeaconRecord>
-					 * <Mac_Address>F6:CC:5E:1A:67:43</Mac_Address>
-					 * <RSS>-83</RSS> <Scan_Id>1</Scan_Id> </BeaconRecord>
-					 * </Bluetooth>
-					 */
-					serializer.startTag("", "Bluetooth");
-					{
-						for (Beacon beacon : beaconList) {
-							serializer.startTag("", "BeaconRecord");
-							{
-								serializer.startTag("", "Mac_Address");
-								serializer.text(beacon.getMacAddress());
-								serializer.endTag("", "Mac_Address");
-								
-								serializer.startTag("", "RSS");
-								serializer.text(beacon.getRssi()+"");
-								serializer.endTag("", "RSS");
-							}
-							serializer.endTag("", "BeaconRecord");
+						InputStream fis = new ByteArrayInputStream(
+								translateToXmlString.getBytes("UTF-8"));
+
+						byte[] temp = new byte[1024 * 4]; // the common size of Internet
+						// transmission
+						int count;
+
+						while ((count = fis.read(temp)) != -1) { // if the xmlFile is
+																	// read over,
+																	// return-1
+							os.write(temp, 0, count);
 						}
+						os.close();
 
+						BufferedReader in = new BufferedReader(new InputStreamReader(
+								httpUrlConnection.getInputStream()));
+						String buf = null;
+
+						while ((buf = in.readLine()) != null) {
+							Log.e(TAG,buf);
+//							response = response + buf + "\n";
+						}
+						
+						in.close();
+						fis.close();
+
+					} catch (MalformedURLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-					serializer.endTag("", "Bluetooth");
-
 				}
-
-				serializer.endTag("", "data");
-				serializer.endDocument();
-				Toast.makeText(this, writer.toString(), Toast.LENGTH_LONG)
-						.show();
-				Log.d(TAG, writer.toString());
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
+			});
+			
+			
 		}
 
 		this.beaconList = new ArrayList<Beacon>();
 		changeNumInTextViewNumber(0);
 		changeNumInTextViewUploadMsg(beaconList.size(), position);
+	}
+
+	private static String translateToXmlString(List<Beacon> beaconList,
+			int position) {
+
+		XmlSerializer serializer = Xml.newSerializer();
+		StringWriter writer = new StringWriter();
+
+		try {
+			serializer.setOutput(writer);
+			serializer.startDocument("UTF-8", true);
+			serializer.startTag("", "data");
+
+			{
+				// <RP_Id>1</RP_Id>
+				serializer.startTag("", "RP_Id");
+				serializer.text(position + "");
+				serializer.endTag("", "RP_Id");
+
+				/*
+				 * <Bluetooth> <BeaconRecord>
+				 * <Mac_Address>DF:81:E2:C5:A2:BA</Mac_Address> <RSS>-79</RSS>
+				 * <Scan_Id>1</Scan_Id> </BeaconRecord> <BeaconRecord>
+				 * <Mac_Address>F6:CC:5E:1A:67:43</Mac_Address> <RSS>-83</RSS>
+				 * <Scan_Id>1</Scan_Id> </BeaconRecord> </Bluetooth>
+				 */
+				serializer.startTag("", "Bluetooth");
+				{
+					for (Beacon beacon : beaconList) {
+						serializer.startTag("", "BeaconRecord");
+						{
+							serializer.startTag("", "Mac_Address");
+							serializer.text(beacon.getMacAddress());
+							serializer.endTag("", "Mac_Address");
+
+							serializer.startTag("", "Scan_Id");
+							serializer.text(1 + "");
+							serializer.endTag("", "Scan_Id");
+							
+							serializer.startTag("", "RSS");
+							serializer.text(beacon.getRssi() + "");
+							serializer.endTag("", "RSS");
+						}
+						serializer.endTag("", "BeaconRecord");
+					}
+
+				}
+				serializer.endTag("", "Bluetooth");
+
+			}
+
+			serializer.endTag("", "data");
+			serializer.endDocument();
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return writer.toString();
 	}
 
 	private void changeNumInTextViewUploadMsg(int size, int position) {
